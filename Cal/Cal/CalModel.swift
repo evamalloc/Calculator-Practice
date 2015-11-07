fda8ea19c3a1488e28c99b4924331243b6cd0aa5
evamalloc <evamalloc@gmail.com>
2015-11-07 23:10:58 +0800

    add project Cal

---
+++ Cal/Cal/CalModel.swift
@@ -0,0 +1,141 @@
+//
+//  CalModel.swift
+//  Cal
+//
+//  Created by Yang Tian on 11/3/15.
+//  Copyright © 2015 Jiao Chu. All rights reserved.
+//
+
+import Foundation
+
+class CalModel {
+    
+    //MARK: emum op
+    private enum Op: CustomStringConvertible {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        //case LeftBracket
+        //case RightBracket
+        
+        
+        //for display
+        var description: String {
+            switch self {
+            case .Operand(let operand):
+                return "\(operand)"
+            case .UnaryOperation(let operation, _):
+                return operation
+            case .BinaryOperation(let operation, _):
+                return operation
+//            case .LeftBracket:
+//                return "("
+//            case .RightBracket:
+//                return ")"
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    //knowOps: operation symbol and the fun of that symbol
+    private var knowOps = [String: Op]()
+    init() {
+        knowOps["+"] = Op.BinaryOperation("+", +)
+        knowOps["−"] = Op.BinaryOperation("−", { $1 - $0 })
+        knowOps["×"] = Op.BinaryOperation("×", *)
+        knowOps["÷"] = Op.BinaryOperation("÷", { $1 / $0})
+        knowOps["√"] = Op.UnaryOperation("√", sqrt)
+        knowOps["sin"] = Op.UnaryOperation("sin", sin)//this fun seems not proper
+        knowOps["cos"] = Op.UnaryOperation("cos", cos)
+        knowOps["tan"] = Op.UnaryOperation("tan", tan)
+    }
+    
+    //
+    private func cal(ops: [Op]) -> (result: Double?, leftOver: [Op]) {
+        if !ops.isEmpty {
+            var leftOver = ops
+            
+            let op = leftOver.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, leftOver) //get number and the rest stack
+            case .UnaryOperation( _, let operation):
+                let operandEvaluation = cal(leftOver)
+                if let operand = operandEvaluation.result {
+                    return ((operation(operand)), operandEvaluation.leftOver)
+                }
+            case .BinaryOperation( _, let operation):
+                let op1Evaluation = cal(leftOver)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = cal(op1Evaluation.leftOver)
+                    if let operand2 = op2Evaluation.result {
+                        return(operation(operand1, operand2), op2Evaluation.leftOver)
+                    }
+                }
+            }
+        }
+        return (nil, ops)//failer
+    }
+    
+    //print
+    func cal() -> Double? {
+        let (result, leftOver) = cal(opStack)
+        print("\(opStack): result is \(result!) and leftover is \(leftOver)")
+        return result
+    }
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return cal()
+    }
+    func performCal(symbol: String) -> Double? {
+        if let operation = knowOps[symbol] {
+            opStack.append(operation)
+        }
+        return cal()
+    }
+    
+    func emptyStack() {
+        opStack.removeAll()
+    }
+//    //
+//    var program: AnyObject {
+//        get { //get the string version of enum Op
+//            var returnValue = Array<String>()
+//            for op in opStack {
+//                returnValue.append(op.description)
+//            }
+//            return returnValue
+//        }
+//        set { //get enum Op from string version
+//            if let opSymbols = newValue as? Array<String> {
+//                var newOpstack = [Op]()
+//                for symbol in opSymbols {
+//                    if let op = knowOps[symbol] {
+//                        newOpstack.append(op)
+//                    } else if let operand = NSNumberFormatter().numberFromString(symbol)?.doubleValue {
+//                        newOpstack.append(.Operand(operand))
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+    
+    
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Cal/Cal/CalViewController.swift
@@ -0,0 +1,80 @@
+//
+//  CalViewController.swift
+//  Cal
+//
+//  Created by Yang Tian on 11/3/15.
+//  Copyright © 2015 Jiao Chu. All rights reserved.
+//
+
+import UIKit
+
+class CalViewController: UIViewController {
+    
+    @IBOutlet weak var screen: UILabel!
+    var userTying: Bool = false
+    var cal = CalModel()
+    var space = ""
+    // convert string to value and oppsite
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(screen.text!)!.doubleValue
+        }
+        set {
+            screen.text = "\(newValue)"
+            userTying = false
+        }
+    }
+    
+//    var operandStack = Array<Double>()
+   
+    
+    @IBAction func number(sender: UIButton) {
+        let number = sender.currentTitle!
+        if userTying {
+            screen.text = screen.text! + number
+        } else {
+            screen.text = number
+            userTying = true
+        }
+    }
+    
+    @IBAction func operation(sender: UIButton) {
+        if userTying {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = cal.performCal(operation) {
+                displayValue = result
+                print(displayValue)
+            } else {
+                displayValue = 0
+                print(displayValue)
+            }
+        }
+    }
+
+
+    @IBAction func enter() {
+        userTying = false
+        if let result = cal.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+   
+    @IBAction func clear() {
+        userTying = false
+        displayValue = 0
+        screen.text = space
+        cal.emptyStack()
+    }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        //screen.text = space
+//    }
+ 
+
+
+}
